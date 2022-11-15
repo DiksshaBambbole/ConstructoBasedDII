b8a55c0f0e8b9b43676c64abdecb7bd5bd6f193c
unknown <diksshan999@gmail.com>
2022-11-15 15:01:37 +0530

    Shweta changes

--- src/main/java/com/example/demo/ConstructorBasedDiiApplication.java
+++ src/main/java/com/example/demo/ConstructorBasedDiiApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConstructorBasedDiiApplication {
 String s="Diksha";
+//Shweta changes
+int i=1;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConstructorBasedDiiApplication.class, args);
