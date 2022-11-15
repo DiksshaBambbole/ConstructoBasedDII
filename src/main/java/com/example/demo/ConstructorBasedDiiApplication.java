b18a380ac15fa8f4441e36b2c018880cfe5957d3
unknown <diksshan999@gmail.com>
2022-11-15 15:17:12 +0530

    added method

--- src/main/java/com/example/demo/ConstructorBasedDiiApplication.java
+++ src/main/java/com/example/demo/ConstructorBasedDiiApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConstructorBasedDiiApplication {
+<<<<<<< Updated upstream
 String s="Diksha";
+=======
+int i=2;
+>>>>>>> Stashed changes
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConstructorBasedDiiApplication.class, args);
